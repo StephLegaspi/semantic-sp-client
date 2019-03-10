7e7f17aedb5448af45901a06111b4f0ded01113e
StephLegaspi <sylegaspi@up.edu.ph>
2019-03-10 17:50:33 +0800

    Integrated SuccessModal when adding request

---
+++ src/components/infoModal/SuccessModal.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+
+import DeleteButton from '../button/DeleteButton.js'
+
+import '../../styles/delete.css';
+import '../../styles/button.css';
+
+class DeleteModal extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			activeModal: this.props.modalStatus,
+		}
+	}
+
+
+	cancel = () => {
+		this.setState({activeModal: false});
+		this.props.changeSuccess();
+	}
+
+	render(){
+		return(
+		<div>
+      	{this.state.activeModal && (
+	      	<div className='delete-modal'>
+	      		<div className='open-delete'>
+
+					<h4> Request has been successfuly sent! </h4>
+	                <br/>
+				    <Button type='submit' onClick={this.cancel} id='cancel-button'>Okay</Button>
+				</div>
+	      	</div>)}
+      	</div>
+		);
+	}
+}
+
+export default DeleteModal;
