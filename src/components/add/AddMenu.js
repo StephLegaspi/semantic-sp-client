3cf9189e8081aebb54a4d0422c5b9ef4de70a177
StephLegaspi <sylegaspi@up.edu.ph>
2019-03-21 00:47:19 +0800

    Added form validation

--- src/components/add/AddMenu.js
+++ src/components/add/AddMenu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Form, Input, TextArea } from 'semantic-ui-react'
+import { Button, Form, Message } from 'semantic-ui-react'
 
 import AddButton from '../button/AddButton.js'
 
@@ -18,7 +18,19 @@ export default class AddPackage extends Component {
         dessert: "",
         soup: "",
         beverage: "",
-        others: ""
+        others: "",
+
+        name_error: '',
+        main_course_error: '',
+        appetizer_error: '',
+        dessert_error: '',
+        soup_error: '',
+        beverage_error: '',
+        others_error: '',
+
+        form_complete: '',
+        prompt_message: '',
+        prompt_header: ''
       }
       this.handleNameChange = this.handleNameChange.bind(this);
       this.handleMainCourseChange = this.handleMainCourseChange.bind(this);
@@ -29,13 +41,13 @@ export default class AddPackage extends Component {
       this.handleOthersChange = this.handleOthersChange.bind(this);
   }
 
-  handleNameChange(e) { this.setState({name: e.target.value}); }
-  handleMainCourseChange(e) { this.setState({main_course: e.target.value}); }
-  handleAppetizerChange(e) { this.setState({appetizer: e.target.value}); }
-  handleDessertChange(e) { this.setState({dessert: e.target.value}); }
-  handleSoupChange(e) { this.setState({soup: e.target.value}); }
-  handleBeverageChange(e) { this.setState({beverage: e.target.value}); }
-  handleOthersChange(e) { this.setState({others: e.target.value}); }
+  handleNameChange(e) { this.setState({name: e.target.value, name_error: false}); }
+  handleMainCourseChange(e) { this.setState({main_course: e.target.value, main_course_error: false}); }
+  handleAppetizerChange(e) { this.setState({appetizer: e.target.value, appetizer_error: false}); }
+  handleDessertChange(e) { this.setState({dessert: e.target.value, dessert_error: false}); }
+  handleSoupChange(e) { this.setState({soup: e.target.value, soup_error: false}); }
+  handleBeverageChange(e) { this.setState({beverage: e.target.value, beverage_error: false}); }
+  handleOthersChange(e) { this.setState({others: e.target.value, others_error: false}); }
 
   onModal = () => {
     this.setState({activeModal: true});
@@ -43,6 +55,50 @@ export default class AddPackage extends Component {
 
   cancel = () => {
     this.setState({activeModal: false});
+  }
+
+  checkForm = () => {
+    var error = false;
+
+    if(this.state.name === ''){
+      this.setState({name_error: true});
+      error=true;
+    }
+    if(this.state.main_course === ''){
+      this.setState({main_course_error: true});
+      error=true;
+    }
+    if(this.state.appetizer === ''){
+      this.setState({appetizer_error: true});
+      error=true;
+    }
+    if(this.state.dessert === ''){
+      this.setState({dessert_error: true});
+      error=true;
+    }
+    if(this.state.soup === ''){
+      this.setState({soup_error: true});
+      error=true;
+    }
+    if(this.state.beverage === ''){
+      this.setState({beverage_error: true});
+      error=true;
+    }
+    if(this.state.others === ''){
+      this.setState({others_error: true});
+      error=true;
+    }
+
+
+    if(error){
+      this.setState({form_complete: false});
+      this.setState({prompt_header: 'Incomplete Information'}); 
+      this.setState({prompt_message: 'Please fill up all the required fields.'});  
+    }else{
+      this.setState({form_complete: true});
+      this.handleSubmit();
+    }
+
   }
 
   handleSubmit = () => {
@@ -74,42 +130,22 @@ export default class AddPackage extends Component {
         {this.state.activeModal && (
           <div className='add-modal'>
             <Form className='form-style-longer'>
-              <Form.Field>
-                  <label>Food Menu Name</label>
-                  <Input placeholder='Food Menu Name' onChange={this.handleNameChange}/>
-                </Form.Field>
+                
+                <Form.Input required label='Food Menu Name' placeholder='Food Menu Name' onChange={this.handleNameChange} error={this.state.name_error}/>
 
                 <Form.Group widths='equal'>
-                  <Form.Field>
-                    <label>Main Course</label>
-                    <TextArea placeholder='e.g. Course1, Course2, Course3' style={{ minHeight: 100 }} onChange={this.handleMainCourseChange}/>
-                  </Form.Field>
-                  <Form.Field>
-                    <label>Appetizer</label>
-                    <TextArea placeholder='e.g. Appetizer1, Appetizer2, Appetizer3' style={{ minHeight: 100 }} onChange={this.handleAppetizerChange}/>
-                  </Form.Field>
+                  <Form.TextArea required label='Main Course' placeholder='e.g. Course1, Course2, Course3' style={{ minHeight: 100 }} onChange={this.handleMainCourseChange} error={this.state.main_course_error}/>
+                  <Form.TextArea required label='Appetizer' placeholder='e.g. Appetizer1, Appetizer2, Appetizer3' style={{ minHeight: 100 }} onChange={this.handleAppetizerChange} error={this.state.appetizer_error}/>
                 </Form.Group>
 
                 <Form.Group widths='equal'>
-                  <Form.Field>
-                    <label>Dessert</label>
-                    <TextArea placeholder='e.g. Dessert1, Dessert2, Dessert3' style={{ minHeight: 100 }} onChange={this.handleDessertChange}/>
-                  </Form.Field>
-                  <Form.Field>
-                    <label>Soup</label>
-                    <TextArea placeholder='e.g. Soup1, Soup2, Soup3' style={{ minHeight: 100 }} onChange={this.handleSoupChange}/>
-                  </Form.Field>
+                  <Form.TextArea required label='Dessert' placeholder='e.g. Dessert1, Dessert2, Dessert3' style={{ minHeight: 100 }} onChange={this.handleDessertChange} error={this.state.dessert_error}/>
+                  <Form.TextArea required label='Soup' placeholder='e.g. Soup1, Soup2, Soup3' style={{ minHeight: 100 }} onChange={this.handleSoupChange} error={this.state.soup_error}/>
                 </Form.Group>
 
                 <Form.Group widths='equal'>
-                  <Form.Field>
-                    <label>Beverage</label>
-                    <TextArea placeholder='e.g. Beverage1, Beverage2, Beverage3' style={{ minHeight: 100 }} onChange={this.handleBeverageChange}/>
-                  </Form.Field>
-                  <Form.Field>
-                    <label>Others</label>
-                    <TextArea placeholder='e.g. Others1, Others2, Others3' style={{ minHeight: 100 }} onChange={this.handleOthersChange}/>
-                  </Form.Field>
+                  <Form.TextArea required label='Beverage' placeholder='e.g. Beverage1, Beverage2, Beverage3' style={{ minHeight: 100 }} onChange={this.handleBeverageChange} error={this.state.beverage_error}/>
+                  <Form.TextArea required label='Others' placeholder='e.g. Others1, Others2, Others3' style={{ minHeight: 100 }} onChange={this.handleOthersChange} error={this.state.others_error}/>
                 </Form.Group>
 
                 <Form.Group inline>
@@ -125,7 +161,14 @@ export default class AddPackage extends Component {
                   </Form.Field>
                 </Form.Group>  
 
-              <Button type='submit' onClick={this.handleSubmit} id='edit-button2'>Add</Button>
+              {(this.state.form_complete===false) ?
+                  <Message
+                    header={this.state.prompt_header}
+                    content={this.state.prompt_message}
+                  />
+                : ''}
+
+              <Button type='submit' onClick={this.checkForm} id='edit-button2'>Add</Button>
               <Button type='submit' onClick={this.cancel} id='cancel-button'>Cancel</Button>
           </Form>
           </div>)}
