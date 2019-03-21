6dcc7a6439106e84fe6ff2013e1aaa31606075b7
StephLegaspi <sylegaspi@up.edu.ph>
2019-03-21 18:40:38 +0800

    Added form validation

--- src/components/add/AddOrder.js
+++ src/components/add/AddOrder.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Form, Input } from 'semantic-ui-react'
+import { Button, Form, Message } from 'semantic-ui-react'
 
 import '../../styles/add.css';
 import '../../styles/button.css';
@@ -18,7 +18,19 @@ export default class AddOrder extends Component {
       email_address: "",
       contact_number: "",
       delivery_address: "",
-      zip_code: ""
+      zip_code: "",
+
+      fname_error: '',
+      mname_error: '',
+      lname_error: '',
+      email_error: '',
+      contact_error: '',
+      delivery_address_error: '',
+      zip_code_error: '',
+
+      form_complete: '',
+      prompt_message: '',
+      prompt_header: ''      
     };
 
     this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
@@ -31,13 +43,13 @@ export default class AddOrder extends Component {
     this.handleCartChange = this.handleCartChange.bind(this);
   }
  
-  handleFirstNameChange(e) { this.setState({first_name: e.target.value}); }
-  handleMiddleNameChange(e) { this.setState({middle_name: e.target.value}); }
-  handleLastNameChange(e) { this.setState({last_name: e.target.value}); }
-  handleEmailChange(e) { this.setState({email_address: e.target.value}); }
-  handleContactChange(e) { this.setState({contact_number: e.target.value}); }
-  handleAddressChange(e) { this.setState({delivery_address: e.target.value}); }
-  handleZipCodeChange(e) { this.setState({zip_code: e.target.value}); }
+  handleFirstNameChange(e) { this.setState({first_name: e.target.value, fname_error: false}); }
+  handleMiddleNameChange(e) { this.setState({middle_name: e.target.value, mname_error: false}); }
+  handleLastNameChange(e) { this.setState({last_name: e.target.value, lname_error: false}); }
+  handleEmailChange(e) { this.setState({email_address: e.target.value, email_error: false}); }
+  handleContactChange(e) { this.setState({contact_number: e.target.value, contact_error: false}); }
+  handleAddressChange(e) { this.setState({delivery_address: e.target.value, delivery_address_error: false}); }
+  handleZipCodeChange(e) { this.setState({zip_code: e.target.value, zip_code_error: false}); }
   handleCartChange(e) { this.setState({cart_id: e.target.value}); }
 
 
@@ -47,6 +59,56 @@ export default class AddOrder extends Component {
 
   cancel = () => {
     this.setState({activeModal: false});
+  }
+
+  checkForm = () => {
+    var error = false;
+
+    if(this.state.first_name === ''){
+      this.setState({fname_error: true});
+      error=true;
+    }
+    if(this.state.middle_name === ''){
+      this.setState({mname_error: true});
+      error=true;
+    }
+    if(this.state.last_name === ''){
+      this.setState({lname_error: true});
+      error=true;
+    }
+    if(this.state.email_address === ''){
+      this.setState({email_error: true});
+      error=true;
+    }
+    if(this.state.contact_number === ''){
+      this.setState({contact_error: true});
+      error=true;
+    }
+    if(this.state.delivery_address === ''){
+      this.setState({delivery_address_error: true});
+      error=true;
+    }
+    if(this.state.zip_code === ''){
+      this.setState({zip_code_error: true});
+      error=true;
+    }
+
+    if(error){
+      this.setState({form_complete: false});
+      this.setState({prompt_header: 'Incomplete Information'}); 
+      this.setState({prompt_message: 'Please fill up all the required fields.'});  
+    }else{
+      this.setState({form_complete: true});
+      this.handleSubmit();
+      this.setState({first_name: ''});
+      this.setState({middle_name: ''});
+      this.setState({last_name: ''});
+      this.setState({email_address: ''});
+      this.setState({contact_number: ''});
+      this.setState({delivery_address: ''});
+      this.setState({zip_code: ''});
+    }
+
   }
 
   handleSubmit = () => {
@@ -83,46 +145,30 @@ export default class AddOrder extends Component {
           <div className='add-modal'>
             <Form className='form-style-smaller'>
                   
-                <Form.Group widths='equal'>
-                  <Form.Field>
-                    <label>First name</label>
-                    <Input placeholder='First name' onChange={this.handleFirstNameChange}/>
-                  </Form.Field>
-
-                  <Form.Field>
-                    <label>Middle name</label>
-                    <Input placeholder='Middle name' onChange={this.handleMiddleNameChange}/>
-                  </Form.Field>
-
-                  <Form.Field>
-                   <label>Last name</label>
-                    <Input placeholder='Last name' onChange={this.handleLastNameChange}/>
-                  </Form.Field>
-                </Form.Group>
-                <Form.Group widths='equal'>
-                  <Form.Field>
-                    <label>Email Address</label>
-                    <Input placeholder='Email Address' onChange={this.handleEmailChange}/>
-                  </Form.Field>
-
-                  <Form.Field>
-                    <label>Contact Number</label>
-                    <Input placeholder='Contact Number' onChange={this.handleContactChange}/>
-                  </Form.Field>
+                <Form.Group widths='equal'>   
+                  <Form.Input required label='First name' placeholder='First name' onChange={this.handleFirstNameChange} error={this.state.fname_error}/>
+                  <Form.Input required label='Middle name' placeholder='Middle name' onChange={this.handleMiddleNameChange} error={this.state.mname_error}/>
+                  <Form.Input required label='Last name' placeholder='Last name' onChange={this.handleLastNameChange} error={this.state.lname_error}/>
                 </Form.Group>
 
                 <Form.Group widths='equal'>
-                  <Form.Field>
-                    <label>Delivery Address</label>
-                    <Input placeholder='Delivery Address' onChange={this.handleAddressChange}/>
-                  </Form.Field>
-                  <Form.Field>
-                    <label>Zip Code</label>
-                    <Input placeholder='Zip Code' onChange={this.handleZipCodeChange}/>
-                  </Form.Field>
+                  <Form.Input required label='Email Address' placeholder='Email Address' onChange={this.handleEmailChange} error={this.state.email_error}/>
+                  <Form.Input required label='Contact Number' placeholder='Contact Number' onChange={this.handleContactChange} error={this.state.contact_error}/>
                 </Form.Group>
 
-              <Button type='submit' onClick={this.handleSubmit} id='edit-button2'>Add</Button>
+                <Form.Group widths='equal'>
+                  <Form.Input required label='Delivery Address' placeholder='Delivery Address' onChange={this.handleAddressChange} error={this.state.delivery_address_error}/>
+                  <Form.Input required label='Zip Code' placeholder='Zip Code' onChange={this.handleZipCodeChange} error={this.state.zip_code_error}/>
+                </Form.Group>
+
+              {(this.state.form_complete===false) ?
+                  <Message
+                    header={this.state.prompt_header}
+                    content={this.state.prompt_message}
+                  />
+                : ''}
+
+              <Button type='submit' onClick={this.checkForm} id='edit-button2'>Add</Button>
               <Button type='submit' onClick={this.cancel} id='cancel-button'>Cancel</Button>
           </Form>
           </div>)}
