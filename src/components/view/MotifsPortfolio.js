7b4c63125731b338199e6b84049eb9c4a2011294
StephLegaspi <sylegaspi@up.edu.ph>
2019-03-23 22:11:00 +0800

    Added fetching image

--- src/components/view/MotifsPortfolio.js
+++ src/components/view/MotifsPortfolio.js
@@ -9,7 +9,6 @@ import Footer from '../footer/Footer.js'
 import '../../styles/view.css';
 import '../../styles/button.css';
 import '../../styles/font.css';
-import img from '../../images/header2.jpg'
 
 class MotifsPortfolio extends Component {
 	constructor(props){
@@ -17,7 +16,12 @@ class MotifsPortfolio extends Component {
 
 		this.state = {
 			motif_name: '',
-			motif_desc: ''
+			motif_desc: '',
+			image1: '',
+			image2: '',
+			image3: '',
+			image4: '',
+			image5: ''
 		}
 	}
 
@@ -34,9 +38,19 @@ class MotifsPortfolio extends Component {
         }).then(function(result) {
             self.setState({motif_name: result.data[0].name});
             self.setState({motif_desc: result.data[0].description});
+            self.setState({image1: result.data[0].image});
+            self.setState({image2: result.data[1].image});
+            self.setState({image3: result.data[2].image});
+            self.setState({image4: result.data[3].image});
+            self.setState({image5: result.data[4].image});
         }).catch(err => {
         	console.log(err);
         })
+
+     
+
+        
+
     }
 
 	render() {
@@ -44,7 +58,7 @@ class MotifsPortfolio extends Component {
 			<div>
 				<Segment id='container-header'>
 		          <HeaderBar headerTitle={this.state.motif_name}/>
-				  <Image size='large' src={img} style={{ minWidth:'100%'}}/>
+				  <Image size='large' src={`http://localhost:3001/${this.state.image1}`} style={{ minWidth:'100%'}}/>
 		          <div id='desc-header'>
 		            <p className='header-font2'> {this.state.motif_desc}  </p>
 		          </div>
@@ -52,10 +66,10 @@ class MotifsPortfolio extends Component {
 				
 				<div id='card-pics'>
 					<Card.Group itemsPerRow={2} >
-					    <Card image={img}/>
-					    <Card image={img} />
-					    <Card image={img} />
-					    <Card image={img} />
+					    <Card image={`http://localhost:3001/${this.state.image2}`}/>
+					    <Card image={`http://localhost:3001/${this.state.image3}`} />
+					    <Card image={`http://localhost:3001/${this.state.image4}`} />
+					    <Card image={`http://localhost:3001/${this.state.image5}`} />
 					    
 					    <CardPackage/>
 				        <CardRequest />
