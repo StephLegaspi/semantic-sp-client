dd7f1a2e91137c4ee2ed858a05531b393a89688f
StephLegaspi <sylegaspi@up.edu.ph>
2019-03-25 02:27:42 +0800

    Integrated AddCustomer

--- src/components/add/AddCustomer.js
+++ src/components/add/AddCustomer.js
@@ -31,7 +31,7 @@ export default class SignUp extends Component {
       last_name: '',
       email_address: '',
       contact_number: '',
-      passsword: '',
+      password: '',
       repeat_password: '',
       address: '',
       zip_code: '',
@@ -56,14 +56,13 @@ export default class SignUp extends Component {
   handleLastNameChange(e) { this.setState({last_name: e.target.value, lname_error: false}); }
   handleEmailChange(e) { this.setState({email_address: e.target.value, email_error: false}); }
   handleContactChange(e) { this.setState({contact_number: e.target.value, contact_error: false}); }
-  handlePasswordChange(e) { this.setState({passsword: e.target.value, password_error: false}); }
+  handlePasswordChange(e) { this.setState({password: e.target.value, password_error: false}); }
   handleRepeatPassChange(e) { this.setState({repeat_password: e.target.value, repeatpass_error: false}); }
   handleAddressChange(e) { this.setState({address: e.target.value, address_error: false}); }
   handleZipCodeChange(e) { this.setState({zip_code: e.target.value, zipcode_error: false}); }
 
   checkForm = () => {
     var error = false;
-    var match_error = false;
 
     if(this.state.first_name === ''){
       this.setState({fname_error: true});
@@ -85,7 +84,7 @@ export default class SignUp extends Component {
       this.setState({contact_error: true});
       error=true;
     }
-    if(this.state.passsword === ''){
+    if(this.state.password === ''){
       this.setState({password_error: true});
       error=true;
     }
@@ -109,19 +108,63 @@ export default class SignUp extends Component {
       this.setState({prompt_message: 'Please fill up all the fields.'});  
     }else{
       this.setState({form_complete: true});
+      if(this.state.password !== this.state.repeat_password){
+        this.setState({form_error_pass: true});
+        this.setState({prompt_header: 'Passwords do not match'});
+        this.setState({prompt_message: 'Please re-type password.'});
+      }else{
+        this.setState({form_error_pass: false});
+        this.handleSubmit();
+      }
     }
 
-    if(this.state.passsword !== this.state.repeat_password){
-      match_error = true;
-    }
-    if(match_error){
-      this.setState({form_error_pass: false});
-      this.setState({prompt_header: 'Passwords do not match'});
-      this.setState({prompt_message: 'Please re-type password.'});  
-    }else{
-      this.setState({form_error_pass: true});
-    }
+  }
 
+   handleSubmit = () => {
+        const request = JSON.stringify({
+            first_name: this.state.first_name, 
+            middle_name: this.state.middle_name,
+            last_name: this.state.last_name,
+            email_address: this.state.email_address,
+            contact_number: this.state.contact_number,
+            password: this.state.password,
+            address: this.state.address,
+            zip_code: this.state.zip_code,
+            user_type: 'Customer',
+
+        })
+       
+        fetch(`http://localhost:3001/v1/Customers`,{
+            headers: { 'Content-Type': 'application/json' },
+            method: "POST",
+            body: request
+          })
+        .then((response) => {
+          return response.json()
+        })
+        .then((result) => {
+          if(result.status === 200){
+            console.log("Successfully added request");
+            this.setState({first_name: ''});
+            this.setState({middle_name: ''});
+            this.setState({last_name: ''});
+            this.setState({email_address: ''});
+            this.setState({contact_number: ''});
+            this.setState({password: ''});
+            this.setState({address: ''});
+            this.setState({zip_code: ''});
+            this.setState({repeat_password: ''});
+
+            this.setState({form_complete: ''});
+            this.setState({form_error_pass: ''});
+            this.setState({prompt_header: ''});
+            this.setState({prompt_message: ''});
+            this.setState({success: true});
+          }
+        })
+        .catch((e) => {
+          console.log(e)
+        })
   }
 
   render(){
@@ -134,27 +177,27 @@ export default class SignUp extends Component {
             <Form >
 
                 <Form.Group widths='equal'>
-                  <Form.Input fluid label='First name' placeholder='First name' onChange={this.handleFirstNameChange} error={this.state.fname_error} />
-                  <Form.Input fluid label='Middle name' placeholder='Middle name' onChange={this.handleMiddleNameChange} error={this.state.mname_error} />
-                  <Form.Input fluid label='Last name' placeholder='Last name' onChange={this.handleLastNameChange} error={this.state.lname_error} />
+                  <Form.Input fluid label='First name' placeholder='First name' onChange={this.handleFirstNameChange} error={this.state.fname_error} value={this.state.first_name}/>
+                  <Form.Input fluid label='Middle name' placeholder='Middle name' onChange={this.handleMiddleNameChange} error={this.state.mname_error} value={this.state.middle_name}/>
+                  <Form.Input fluid label='Last name' placeholder='Last name' onChange={this.handleLastNameChange} error={this.state.lname_error} value={this.state.last_name}/>
                 </Form.Group>
 
                 <Form.Group widths='equal'>
-                  <Form.Input fluid label='Email Address' placeholder='Email Address' onChange={this.handleEmailChange} error={this.state.email_error} />
-                  <Form.Input fluid label='Contact Number' placeholder='Contact Number' onChange={this.handleContactChange} error={this.state.contact_error} />
+                  <Form.Input fluid label='Email Address' placeholder='Email Address' onChange={this.handleEmailChange} error={this.state.email_error} value={this.state.email_address}/>
+                  <Form.Input fluid label='Contact Number' placeholder='Contact Number' onChange={this.handleContactChange} error={this.state.contact_error} value={this.state.contact_number}/>
                 </Form.Group >
 
                 <Form.Group widths='equal'>
-                  <Form.Input fluid label='Password' placeholder='Password' onChange={this.handlePasswordChange} error={this.state.password_error} />
-                  <Form.Input fluid label='Repeat Password' placeholder='Repeat Password' onChange={this.handleRepeatPassChange} error={this.state.repeatpass_error} />
+                  <Form.Input fluid label='Password' placeholder='Password' onChange={this.handlePasswordChange} error={this.state.password_error} value={this.state.password}/>
+                  <Form.Input fluid label='Repeat Password' placeholder='Repeat Password' onChange={this.handleRepeatPassChange} error={this.state.repeatpass_error} value={this.state.repeat_password}/>
                 </Form.Group>
 
                 <Form.Group>
-                  <Form.Input width={15} fluid label='Address' placeholder='Address' onChange={this.handleAddressChange} error={this.state.address_error} />
-                  <Form.Input width={3} fluid label='Zip Code' placeholder='Zip Code' onChange={this.handleZipCodeChange} error={this.state.zipcode_error} />
+                  <Form.Input width={15} fluid label='Address' placeholder='Address' onChange={this.handleAddressChange} error={this.state.address_error} value={this.state.address}/>
+                  <Form.Input width={3} fluid label='Zip Code' placeholder='Zip Code' onChange={this.handleZipCodeChange} error={this.state.zipcode_error} value={this.state.zip_code}/>
                 </Form.Group>
 
-                {(this.state.form_complete===false || this.state.form_error_pass===false) ?
+                {(this.state.form_complete===false || this.state.form_error_pass===true) ?
                   <Message
                     header={this.state.prompt_header}
                     content={this.state.prompt_message}
