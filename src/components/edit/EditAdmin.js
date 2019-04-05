f83680f0759066c80ca73c4622938b47422d4068
StephLegaspi <sylegaspi@up.edu.ph>
2019-04-05 21:19:22 +0800

    Integrated edit image

--- src/components/edit/EditAdmin.js
+++ src/components/edit/EditAdmin.js
@@ -17,13 +17,15 @@ class EditAdmin extends Component {
       middle_name: "",
       last_name: "",
       email_address: "",
-      contact_number: ""
+      contact_number: "",
+      image: ''
     }
     this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
     this.handleMiddleNameChange = this.handleMiddleNameChange.bind(this);
     this.handleLastNameChange = this.handleLastNameChange.bind(this);
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handleContactChange = this.handleContactChange.bind(this);
+    this.handleImageChange = this.handleImageChange.bind(this);
   }
 
   handleFirstNameChange(e) {this.setState({first_name: e.target.value}); }
@@ -31,6 +33,7 @@ class EditAdmin extends Component {
   handleLastNameChange(e) {this.setState({last_name: e.target.value}); }
   handleEmailChange(e) {this.setState({email_address: e.target.value}); }
   handleContactChange(e) {this.setState({contact_number: e.target.value}); }
+  handleImageChange(e) {this.setState({image: e.target.files[0]});}
 
   onModal = () => {
     this.getData();
@@ -47,17 +50,32 @@ class EditAdmin extends Component {
     this.setState({last_name: this.props.data.last_name})
     this.setState({email_address: this.props.data.email_address})
     this.setState({contact_number: this.props.data.contact_number})
+    this.setState({image: null})
   }
 
   submitEdit = () => {
       const id_session = JSON.parse(local_storage.getItem("user_data")).id;
 
-        const admin = JSON.stringify({first_name: this.state.first_name, middle_name: this.state.middle_name, last_name: this.state.last_name, email_address: this.state.email_address, contact_number: this.state.contact_number, session_id: id_session})
+        let formData = new FormData();
+        formData.set('enctype','multipart/form-data'); 
+
+        formData.append('first_name', this.state.first_name);
+        formData.append('middle_name', this.state.middle_name);
+        formData.append('last_name', this.state.last_name);
+        formData.append('email_address', this.state.email_address);
+        formData.append('contact_number', this.state.contact_number);
+        formData.append('image', this.state.image);
+        formData.append('session_id', id_session);
+       
+        if(this.state.image === null){
+          formData.append('image_changed', false);
+        }else{
+          formData.append('image_changed', true);
+        }
        
         fetch(`http://localhost:3001/v1/administrators/` + this.props.data.id,{
-            headers: { 'Content-Type': 'application/json' },
             method: "PUT",
-            body: admin
+            body: formData
           })
         .then((response) => {
           return response.json()
@@ -108,6 +126,20 @@ class EditAdmin extends Component {
                     <Input placeholder='Contact Number' defaultValue={this.props.data.contact_number} onChange={this.handleContactChange}/>
                   </Form.Field>
                 </Form.Group> 
+
+                <Form.Group inline>
+                    <label>Admin Image: </label>
+                    <Form.Field className="relative">
+                        <input name='image' type="file" className="absolute" onChange={this.handleImageChange} id='embedpollfileinput'/>
+                        <div className="absolute2"> 
+                          <label for="embedpollfileinput" className="ui button" style={{height: '37px', width:'104px', paddingTop: '10px', paddingRight: '17px'}}> 
+                            <i class="ui upload icon"></i>   
+                             Upload
+                          </label>
+                        </div>
+                    </Form.Field>
+                </Form.Group>  
+
                 <Button type='submit' onClick={this.submitEdit} id='edit-button2'>Edit</Button>
                 <Button type='submit' onClick={this.cancel} id='cancel-button'>Cancel</Button>
             </Form>
