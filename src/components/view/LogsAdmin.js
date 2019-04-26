9663114d49e4369ca19be67b232d4a9dfa993381
StephLegaspi <sylegaspi@up.edu.ph>
2019-04-26 17:07:55 +0800

    Removed ID from table

--- src/components/view/LogsAdmin.js
+++ src/components/view/LogsAdmin.js
@@ -91,7 +91,6 @@ class Logs extends Component {
 				<Table>
 				    <Table.Header>
 				      <Table.Row>
-				        <Table.HeaderCell style={{width: '7%'}}>Log ID</Table.HeaderCell>
 				        <Table.HeaderCell style={{width: '7%'}}>Admin ID</Table.HeaderCell>
 				        <Table.HeaderCell style={{width: '20%'}}>Action</Table.HeaderCell>
 				        <Table.HeaderCell style={{width: '20%'}}>Timestamp</Table.HeaderCell>
@@ -102,7 +101,6 @@ class Logs extends Component {
 				    <Table.Body>
 				    {this.state.data.map(log =>
 				      <Table.Row>
-				        <Table.Cell>{log.id}</Table.Cell>
 				        <Table.Cell>{log.user_id}</Table.Cell>
 				        <Table.Cell>{log.action}</Table.Cell>
 				        <Table.Cell>{log.log_timestamp}</Table.Cell>
--- src/components/view/LogsCustomer.js
+++ src/components/view/LogsCustomer.js
@@ -90,7 +90,6 @@ class Logs extends Component {
 				<Table>
 				    <Table.Header>
 				      <Table.Row>
-				        <Table.HeaderCell style={{width: '7%'}}>Log ID</Table.HeaderCell>
 				        <Table.HeaderCell style={{width: '7%'}}>Customer ID</Table.HeaderCell>
 				        <Table.HeaderCell style={{width: '20%'}}>Action</Table.HeaderCell>
 				        <Table.HeaderCell style={{width: '20%'}}>Timestamp</Table.HeaderCell>
@@ -101,7 +100,6 @@ class Logs extends Component {
 				    <Table.Body>
 				    {this.state.data.map(log =>
 				      <Table.Row>
-				        <Table.Cell>{log.id}</Table.Cell>
 				        <Table.Cell>{log.user_id}</Table.Cell>
 				        <Table.Cell>{log.action}</Table.Cell>
 				        <Table.Cell>{log.log_timestamp}</Table.Cell>
